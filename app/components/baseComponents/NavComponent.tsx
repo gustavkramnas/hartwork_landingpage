e6d8afae90d27c1e0ff9b933f384a02e7c8b6186
chrillah <ch.ronnberg@gmail.com>
2024-10-03 18:05:15 +0200

    chore: svg logo added

--- app/components/baseComponents/NavComponent.tsx
+++ app/components/baseComponents/NavComponent.tsx
@@ -1,37 +1,37 @@
-import Link from 'next/link';
-import { theme } from '../../utils/appSettings/theme';
-import { CompanyLogo } from './CompanyLogo';
-import { fetchCompanyAppSetting } from '@/app/utils/contentful/queries/home';
+import Link from 'next/link'
+import { theme } from '../../utils/appSettings/theme'
+import { CompanyLogo } from './CompanyLogo'
+//import { fetchCompanyAppSetting } from '@/app/utils/contentful/queries/home'
 
 export const NavBar = async () => {
-  const companyInformation = await fetchCompanyAppSetting();
+  // const companyInformation = await fetchCompanyAppSetting()
 
-  const imageUrl = companyInformation.fields.image?.fields.file.url
-    ? `https:${companyInformation.fields.image.fields.file.url}`
-    : '';
+  // const imageUrl = companyInformation.fields.image?.fields.file.url
+  //   ? `https:${companyInformation.fields.image.fields.file.url}`
+  //   : ''
 
-  const pages = theme.pages.links;
+  const pages = theme.pages.links
 
   return (
     <header>
       <nav>
         <CompanyLogo
-          imageUrl={imageUrl}
-          companyName={companyInformation.fields.companyName}
+        // imageUrl={imageUrl}
+        // companyName={companyInformation.fields.companyName}
         />
         <ul>
           {pages.map((page) => {
             const title =
-              page.replace('/', '').charAt(0).toUpperCase() + page.slice(1);
+              page.replace('/', '').charAt(0).toUpperCase() + page.slice(1)
 
             return (
               <li key={page}>
                 <Link href={page}>{title}</Link>
               </li>
-            );
+            )
           })}
         </ul>
       </nav>
     </header>
-  );
-};
+  )
+}
