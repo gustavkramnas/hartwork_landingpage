8c3b9af3c89781602f3a70ea51885d0320ddc244
chrillah <ch.ronnberg@gmail.com>
2024-10-04 18:23:52 +0200

    chore: base components updated

--- app/components/baseComponents/base.tsx
+++ app/components/baseComponents/base.tsx
@@ -1,8 +1,14 @@
+import { theme } from '@/app/utils/appSettings/theme'
 import styled from 'styled-components'
 
-export const BaseSection = styled.section`
-  max-width: ${(props) => props.theme.style.layout.maxWidth};
-`
+
 export const BaseMain = styled.main`
   margin: 0 auto;
+`
+export const Container = styled.section`
+  max-width: ${theme.style.layout.sizes.maxWidth};
+`
+
+export const BaseSection = styled(Container)`
+
 `
