3b7f6c9a72467e56bd2453ec8641cba2c92e681a
chrillah <ch.ronnberg@gmail.com>
2024-10-06 15:08:53 +0200

    chore: hero displays title

--- app/components/homePageComponents/DisplayProjectsHero.tsx
+++ app/components/homePageComponents/DisplayProjectsHero.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import styled from 'styled-components'
 import { useState, useEffect } from 'react'
 import { H1 } from '../fontComponents/fonts'
 import { ImageComponent } from '../imageComponents/ImageComponent'
+import { theme } from '@/app/utils/appSettings/theme'
 
 type DisplayProject = {
   imageUrl: string
@@ -13,6 +15,25 @@ type DisplayProject = {
 type Props = {
   displayProjects: DisplayProject[]
 }
+
+const Wrapper = styled.div`
+  position: relative;
+`
+const ProjectInformationWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const ProjectInformation = styled.div`
+  width: 100%;
+  max-width: ${theme.style.layout.sizes.maxWidth};
+`
+
 export const HeroComponent = ({ displayProjects }: Props) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0)
 
@@ -29,10 +50,17 @@ export const HeroComponent = ({ displayProjects }: Props) => {
   return (
     <div>
       {displayProjects.length > 0 ? (
-        <ImageComponent
-          url={displayProjects[currentImageIndex].imageUrl}
-          title={displayProjects[currentImageIndex].title}
-        />
+        <Wrapper>
+          <ImageComponent
+            url={displayProjects[currentImageIndex].imageUrl}
+            title={displayProjects[currentImageIndex].title}
+          />
+          <ProjectInformationWrapper>
+            <ProjectInformation>
+              <H1>{displayProjects[currentImageIndex].title}</H1>
+            </ProjectInformation>
+          </ProjectInformationWrapper>
+        </Wrapper>
       ) : (
         <H1>No project images</H1>
       )}
