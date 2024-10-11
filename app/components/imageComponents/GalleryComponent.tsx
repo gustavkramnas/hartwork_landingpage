1e40968af8a1a5d683fa16fabf51cf53fbd72940
chrillah <ch.ronnberg@gmail.com>
2024-10-11 08:46:11 +0200

    chore: gallery supports video

--- app/components/imageComponents/GalleryComponent.tsx
+++ app/components/imageComponents/GalleryComponent.tsx
@@ -2,19 +2,34 @@ import { GalleryImageComponent } from './GalleryImageComponent'
 import { GalleryComponentProps } from '@/app/types/Types'
 import { GalleryContainer, GalleryItem } from './StyledComponents'
 import { VideoComponent } from './VideoComponent'
+import { P } from '../fontComponents/fonts'
 
 export const GalleryComponent = ({ galleryItems }: GalleryComponentProps) => {
+  const isImageFile = (url: string) => {
+    return url.match(/\.(jpeg|jpg|gif|png|svg)$/i)
+  }
+
+  const isVideoFile = (url: string) => {
+    return url.match(/\.(mp4|webm|ogg)$/i)
+  }
+
   return (
     <GalleryContainer>
-      {galleryItems.map((item, index) => (
-        <GalleryItem key={index}>
-          <GalleryImageComponent
-            url={`https:${item.fields.file.url}`}
-            title={item.fields.title}
-          />
-          {/* <VideoComponent url={`https:${item.fields.file.url}`} /> */}
-        </GalleryItem>
-      ))}
+      {galleryItems.map((item, index) => {
+        const fileUrl = `https:${item.fields.file.url}`
+
+        return (
+          <GalleryItem key={index}>
+            {isImageFile(fileUrl) ? (
+              <GalleryImageComponent url={fileUrl} title={item.fields.title} />
+            ) : isVideoFile(fileUrl) ? (
+              <VideoComponent url={fileUrl} />
+            ) : (
+              <P $white>Unsupported file type</P>
+            )}
+          </GalleryItem>
+        )
+      })}
     </GalleryContainer>
   )
 }
