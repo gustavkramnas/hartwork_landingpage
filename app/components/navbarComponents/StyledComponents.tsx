25fc8c3b31d0e2cc5dd7fede237d7d7cdde1635c
Gustav Kramnäs <gustav@kramnas.se>
2024-10-11 14:08:18 +0200

    changed padding to header

--- app/components/navbarComponents/StyledComponents.tsx
+++ app/components/navbarComponents/StyledComponents.tsx
@@ -29,7 +29,7 @@ export const Header = styled.header<Props>`
   align-items: center;
 
   @media (min-width: ${theme.style.layout.sizes.mobileQueries}) {
-    padding: ${({ $scrolled }) => ($scrolled ? '5px 0' : '70px 0 20px 0')};
+    padding: ${({ $scrolled }) => ($scrolled ? '5px 0' : '20px 0 20px 0')};
     box-shadow: ${({ $scrolled }) =>
       $scrolled
         ? 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px'
