b1f48c2c02a76870b9f8b95a36722c4ff9ef02d6
Gustav Kramnäs <gustav@kramnas.se>
2024-10-14 14:51:02 +0200

    opacity on only the background and not the text in header while hovering

--- app/components/navbarComponents/StyledComponents.tsx
+++ app/components/navbarComponents/StyledComponents.tsx
@@ -29,7 +29,7 @@ export const Header = styled.header<Props>`
   align-items: center;
 
   @media (min-width: ${theme.style.layout.sizes.mobileQueries}) {
-    padding: ${({ $scrolled }) => ($scrolled ? '5px 0' : '70px 0 20px 0')};
+    padding: ${({ $scrolled }) => ($scrolled ? '5px 0' : '35px 0 20px 0')};
     /* box-shadow: ${({ $scrolled }) =>
       $scrolled
         ? 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px'
@@ -39,8 +39,8 @@ export const Header = styled.header<Props>`
       $scrolled ? `${theme.style.colors.tertiary}` : 'transparent'};
     transition: 0.9s;
     &:hover {
-      background-color: ${theme.style.colors.tertiary};
-    }
+    background-color: rgba(255, 255, 255, 0.9); /* Add opacity only to background */
+  }
   }
 
   @media (max-width: ${theme.style.layout.sizes.mobileQueries}) {
