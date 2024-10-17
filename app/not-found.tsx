1c074c03fc413fb3fce896f7f593393de633d688
chrillah <ch.ronnberg@gmail.com>
2024-10-17 23:00:19 +0200

    fix: button and layout in not-found page updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,9 @@
 import {
   MainWithPaddingTop,
-  Container,
   FlexContainer,
   FadeSection
 } from './components/baseComponents/base'
-import { ArrowButton } from './components/buttonComponents/ButtonComponents'
+import { AppButton } from './components/buttonComponents/ButtonComponents'
 import { DisplayH1, P } from './components/fontComponents/fonts'
 
 const NotFound = async () => {
@@ -14,10 +13,13 @@ const NotFound = async () => {
         <FlexContainer>
           <DisplayH1>404</DisplayH1>
         </FlexContainer>
-        <Container>
+
+        <FlexContainer>
           <P>Oops! Sidan hittades inte</P>
-          <ArrowButton href="/" title="Tillbaka till startsidan" />
-        </Container>
+          <br></br>
+          {/* <ArrowButton href="/" title="Tillbaka till startsidan" /> */}
+          <AppButton href="/" title="Till start" />
+        </FlexContainer>
       </FadeSection>
     </MainWithPaddingTop>
   )
